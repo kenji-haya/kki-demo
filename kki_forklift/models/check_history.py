bf05d00b21432d9da5f8226915306fb6965b0e45
kenji-haya <kenji_hayashi@v-kki.co.jp>
2022-09-01 15:03:14 +0900

    修正

--- kki_forklift/models/check_history.py
+++ kki_forklift/models/check_history.py
@@ -9,12 +9,14 @@ class kki_forklift_check_history(models.Model):
     _description = 'kki_forklift.history'
 
     name = fields.Char("name")
+    owner_id = fields.Many2one('res.users', 'owner_id', default=lambda self: self.env.user)
     check_date = fields.Date("check date", required="True", default=datetime.today())
     lift_id = fields.Many2one("kki_forklift.lift", "Forklift")
     image = fields.Binary("image")
     fork_1 = fields.Boolean("【フォーク】亀裂や曲がりはないか")
-    back_1 = fields.Boolean("【バックレスト】")
-    chain_1 = fields.Boolean("【チェーン】")
-    mast_1 = fields.Boolean("【マスト】")
-    tire_1 = fields.Boolean("【タイヤ】")
-    handle_1 = fields.Boolean("【ハンドル】")
+    back_1 = fields.Boolean("【バックレスト】亀裂・変形・取付部に緩みはないか")
+    chain_1 = fields.Boolean("【チェーン】傷・ねじれがなく、張りが均等か")
+    mast_1 = fields.Boolean("【マスト】上昇・下降・前後斜が円滑か")
+    tire_1 = fields.Boolean("【タイヤ】損傷や異常摩耗がないか／ハブナットの緩み、脱落がないか")
+    handle_1 = fields.Boolean("【ハンドル】著しい遊びまたはガタツキがないか")
+
--- kki_forklift/models/forklift.py
+++ kki_forklift/models/forklift.py
@@ -18,3 +18,12 @@ class kki_forklift(models.Model):
         inverse_name="lift_id",
         string="check history")
     price = fields.Integer("price")
+    history_count_2 = fields.Integer("test")
+    history_count = fields.Integer(compute="_compute_check_history_count")
+
+    def _compute_check_history_count(self):
+        for rec in self:
+            print("check")
+            history_count = self.env['kki_forklift.history'].search_count([('lift_id', '=', rec.id)])
+            print("history_count")
+            rec.history_count = history_count
