f04fa3ce4411b5180a7d133aecb2fdfe9a38197e
kenji-haya <kenji_hayashi@v-kki.co.jp>
2022-10-12 11:22:28 +0900

    製造APP　listとformに内部参照追加

--- kki_mrp/models/mrp.py
+++ kki_mrp/models/mrp.py
@@ -10,6 +10,19 @@ class kki_mrp(models.Model):
     # order_date = fields.Datetime("納品日", required=True)
     create_date = fields.Datetime("納品日", required=True)
 
+    default_code = fields.Char(
+        string='default_code', help="default_code from product record")
+
+    # 商品を選んだら内部参照を表示する
+    @api.onchange("product_id")
+    def _onchange_default_code(self):
+        print(self.product_id)
+        self.default_code = self.product_id.default_code
+
+    # @api.onchange("default_code")
+    # def _onchange_product_id(self):
+    #     print(self.product_id)
+    #     self.product_id = self.product_id.name
 #     name = fields.Char()
 #     value = fields.Integer()
 #     value2 = fields.Float(compute="_value_pc", store=True)
