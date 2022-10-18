5cf16a3d933d7d27f0ca01881fcaeccdbe54a617
kei_fujita <kei_fujita@v-kki.co.jp>
2022-10-18 11:03:41 +0900

    入荷日項目の追加（未完成）

--- kki_mrp/models/mrp.py
+++ kki_mrp/models/mrp.py
@@ -8,7 +8,7 @@ class kki_mrp(models.Model):
     # _name = 'kki_mrp.kki_mrp'
     # _description = 'kki_mrp.kki_mrp'
     # order_date = fields.Datetime("納品日", required=True)
-    create_date = fields.Datetime("納品日", required=True)
+    kcreate_date = fields.Datetime("納品日", required=True)
     default_code = fields.Char(
         string='default_code', help="default_code from product record"
     )
@@ -16,12 +16,16 @@ class kki_mrp(models.Model):
         string="Factory", help="menu_1 from product record"
     )
 
+    arrival_day = fields.Char(
+        string='arrival_day', help="date_deadline from stock_move record")
+
     # 商品を選んだら内部参照を表示する
     @api.onchange("product_id")
     def _onchange_default_code(self):
-        if self.default_code:
+        # if self.default_code:
+            self.menu_1 = self.product_id.menu_1.name
             self.default_code = self.product_id.default_code
-        self.menu_1 = self.product_id.menu_1.name
+
 
     # デフォルトコードが更新されたら商品を検索してプロダクトに代入する
     @api.onchange("default_code")
@@ -34,6 +38,21 @@ class kki_mrp(models.Model):
                     rec.product_id = product
             else:
                 rec.product_id = ""
+
+
+    # デフォルトコードが更新されたら商品を検索してプロダクトに代入する
+    # def _arrival_day(self):
+    #     print(self)
+        # for rec in self:
+        #     if rec.arrival_day:
+        #         date = self.env['stock.move'].search([('date_deadline', '=', rec.date_deadline)])
+        #         print(date)
+        #         if date:
+        #             rec.product_id = date
+        #     else:
+        #         rec.product_id = ""
+
+
 
 #     name = fields.Char()
 #     value = fields.Integer()
